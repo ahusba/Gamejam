d9b00d3a903ab26883986ab3a0a523fef1db68d5
Garry Barter <garry@gjbarter.co.uk>
2014-06-30 14:39:09 +0100

    collision detection

---
+++ game1/Assets/Asset/Prefabs/Collision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour {
+	public Collider leftHand;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+ 	void OnTriggerEnter(Collider other)
+		{
+			//if (other.gameObject.name == "TopLeft")
+			//{
+				Debug.Log("TopLeft");
+			//}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+

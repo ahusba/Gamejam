3508e5ac0cd365b4242b11a9343c3b0444e9256c
ahusba <ajhusband@gmail.com>
2014-06-30 21:27:29 +0100

    Level Creation
    can build level classes to use in playing the game

---
+++ game1/Assets/Script/GameController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+public  enum Quadrant{ TopRight, Top, TopLeft, MiddleRight, Middle, MiddleLeft, BottomRight, Bottom, BottomLeft};
+
+public class Move {
+	public 	List<Quadrant> points;
+
+	public Move(){
+		points = new List<Quadrant> ();
+	}
+	public void Add(Quadrant point){
+		points.Add (point);
+	}
+}
+
+public class Level {
+	public List<Move> moves;
+	public int difficulty;
+	public Level(int difficlulty){
+		moves = new List<Move> ();
+		this.difficulty = difficlulty;
+	}
+	public void Add (Move move){
+		moves.Add (move);
+	}
+}
+
+public class GameController : MonoBehaviour {
+
+	private Level CreateLevel (string filename, int diff){
+		Level level = new Level (diff);
+		try{
+			string line;
+			StreamReader theReader = new StreamReader(filename, Encoding.Default);
+			using (theReader){
+				do{
+					line = theReader.ReadLine();
+					if(line != null){
+						string[] entry = line.Split (',');
+						if(entry.Length > 1){
+							if(Convert.ToInt32(entry[0]) == level.difficulty){
+								Move move = new Move();
+								for (int i = 1; i < entry.Length; i++){
+									Quadrant q =(Quadrant) Enum.Parse (typeof(Quadrant),entry[i]);
+									move.Add(q);
+								}
+								level.Add (move);
+							}
+						}
+					}
+				}
+				while(line != null);
+				theReader.Close ();
+			}
+			return level;
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine("{0}\n", e.Message);
+			return null;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		Level level = CreateLevel ("Resources/movelist.txt", 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
